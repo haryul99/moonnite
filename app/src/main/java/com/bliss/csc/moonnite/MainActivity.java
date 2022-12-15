079379e8be1b5d80e108c69204d6dd84a44a7539
haryul99 <dain117@naver.com>
2022-12-15 21:25:36 +0900

    12.15 commit

--- app/src/main/java/com/bliss/csc/moonnite/MainActivity.java
+++ app/src/main/java/com/bliss/csc/moonnite/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.calendar);
         TextView title = findViewById(R.id.date);
         ImageView moonphoto = findViewById(R.id.moonphoto);
+        CalendarView calendarView = findViewById(R.id.calendarView);
 
 
         Date date = new Date();
@@ -30,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         title.setText(dateFormat.format(date));
         imageView.setColorFilter(Color.parseColor("#fffff2"));
 
-        // 캘린더 아이콘 선택 시 액티비티 전환
-       imageView.setOnClickListener(new View.OnClickListener() {
+        // 달 사진 선택 시 액티비티 전환
+        moonphoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CompassActivity.class);
@@ -39,12 +41,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendarView.getRootView().setVisibility(View.VISIBLE);
+            }
+        });
 
-       if (dateFormat.format(date) < 12.14){
-         moonphoto.setImageResource(getDrawable(R.drawable.m6));
-       };
 
+        // 음력 날짜에 대한 달 사진change
+//       if (title < 12.14){
+//         moonphoto.setImageResource(getDrawable(R.drawable.m6));
+//       };
 
 
     }
+
 }
